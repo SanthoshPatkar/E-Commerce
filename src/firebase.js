4255617962671fecbfdbf1444ef9f277d86f0e14
SanthoshPatkar <ssanthoshp159@gmail.com>
2021-12-25 10:49:01 +0530

    Initial update

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAvheyNRrIPnLs2XCYyXELkx6-9Rzn_LVo",
+    authDomain: "amozon-clone-112f4.firebaseapp.com",
+    projectId: "amozon-clone-112f4",
+    storageBucket: "amozon-clone-112f4.appspot.com",
+    messagingSenderId: "1066998201433",
+    appId: "1:1066998201433:web:ac086dd9116cfc0b0e8548",
+    measurementId: "G-EFQR6DGXSW"
+  };
