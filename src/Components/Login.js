5c359a886e0f2bf621b80d432f786142a2cd5fb7
SanthoshPatkar <ssanthoshp159@gmail.com>
2022-01-10 21:44:58 +0530

    login details

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../CSS/login.css";
 import { Link } from "react-router-dom";
+import { auth } from "../firebase";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+  const register = (e) => {
+    e.preventDefault();
+    auth
+      .createuserwithemailandpassword(email, Password)
+      .then((auth) => {
+        console.log(auth);
+      })
+      .catch((error) => error.message);
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -12,6 +28,34 @@ function Login() {
           alt=""
         />
       </Link>
+      <div className="login_container">
+        <h1>Sign in</h1>
+        <form>
+          <h5>E-mail or Mobile Number</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={Password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={signIn} className="login_signin">
+            Sign In
+          </button>
+          <p>
+            By continuing, you agree to Amazon-Clone Conditions of Use and
+            Privacy Notice.
+          </p>
+        </form>
+      </div>
+      <p>New to Amazon?</p>
+      <button type="submit" onClick={register} className="login_register">
+        create your amazon account
+      </button>
     </div>
   );
 }
